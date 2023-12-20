6417bb4ade1f42abfe9b7eab5fd450d5e88a6c8c
nhardbalansag <nhardbalansag@gmail.com>
2023-12-21 01:23:33 +0800

    update routes

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ Route::group(['middleware' => 'auth:api'], function() {
             Route::prefix('report')->group(function () {
                 Route::post('/create/activity', [ReportController::class, 'CreateActivityReport']);
                 Route::post('/generate/pdf', [ReportController::class, 'GeneratePDFReport']);
-                Route::get('/filter/date', [ReportController::class, 'FilterDateRangeReport']);
+                Route::post('/filter/date', [ReportController::class, 'FilterDateRangeReport']);
             });
 
             Route::prefix('dashboard')->group(function () {
